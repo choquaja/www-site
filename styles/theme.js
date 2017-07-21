28713cbafd8dfb616083a1afe25043d5d1f3734f
choquaja <choquaja@whitman.edu>
2017-07-21 11:58:17 -0400

    added styles to theme

---
+++ styles/theme.js
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    greylight: '#F0F1F1',
+    greybase: '#31383F',
+    greyblue: '#182752',
+    orangelight: '#FAAC18',
+    orangebase: '#F26522',
+    orangedark: '#F26522',
+    graphitebase: '#3B3B3B',
+    graphitebase2: '#5E5E5E',
+    graphitemid: '#8C8C8C',
+  },
+
+  fonts: {
+    headings: '"filson-soft", sans-serif',
+    body: '"Fira Sans, sans-serif',
+  },
+
+  units: {
+    base: 10,
+    basetwo: 10,
+    gutter: '20px',
+    globalWidth: '1440px',
+    headerheight: '88px',
+  },
+}
