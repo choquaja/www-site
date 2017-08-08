39f843dd7b49bb6464bc621b76aaf1a16f537a5b
choquaja <choquaja@whitman.edu>
2017-08-07 22:00:09 -0400

    reorganizing code

--- components/content-section/content-section.js
+++ components/content-section/content-section.js
@@ -9,21 +9,6 @@ const ContentSectionMain = styled.div`
   background: white;
 `
 
-const NoPaddingMobile = styled.div`
-  ${media.tablet`
-    .content-section-wrapper {
-      padding-left: 0;
-      padding-right: 0;
-    }
-    .content-section-header {
-      padding-left: $gutter*2;
-      padding-right: $gutter*2;
-      min-width: 100% !important;
-      max-width: 100% !important;
-    }
-  }`};
-`
-
 const ContentWrapper = styled.div`
   opacity: 0;
   padding-top: $gutter*2;
@@ -33,7 +18,9 @@ const ContentWrapper = styled.div`
   ${globalWrapper} ${media.desktop`
   padding-top: ${props => props.theme.units.gutter * 4.5};
   padding-bottom: ${props => props.theme.units.gutter * 4.5};
-`};
+  `} ${media.tablet`
+      padding-left: 0;
+      padding-right: 0;`};
 `
 
 const ContentHeader = styled.div`
@@ -42,6 +29,12 @@ const ContentHeader = styled.div`
     ${globalWrapper}
     padding-left:${props => props.theme.units.gutter}
     padding-right:${props => props.theme.units.gutter}
+  `};
+  ${media.tablet`
+    padding-left: $gutter*2;
+    padding-right: $gutter*2;
+    min-width: 100% !important;
+    max-width: 100% !important;
   `};
 `
 
@@ -70,7 +63,16 @@ const ContentBodyWrapper = styled.div`
   padding-bottom: ${props => props.theme.units.unit}px;
 `
 
-const BodyULinP = styled.p`
+const BodyUL = styled.ul`
+  margin: 0;
+  font-size: 18px;
+  line-height: 30px;
+  font-weight: 200;
+  ${media.desktop`max-width: 85%`};
+  ${media.handheld`text-align: left`};
+`
+
+const BodyP = styled.ul`
   margin: 0;
   font-size: 18px;
   line-height: 30px;
@@ -97,16 +99,17 @@ const ContentGraphicLabel = styled.div`
   padding-top: ${props => prop.theme.units.unit}px;
 `
 
-const ContentGraphicImage = styled.div`background: white;`
-
-// line 109 img {}
+const ContentGraphicImage = styled.div`
+  background: white;
+  max-width: 100%;
+  display: block;
+  box-shadow: ${props => props.theme.animations.dropShadowxlLight};
+`
 
 const CenterAlignPInContentSectionBody = styled.p`
   max-width: 680px;
   ${globalWrapper};
 `
-
-//line 130 [white]
 
 const CenterP = styled.p`
   font-size: 22px;
@@ -122,19 +125,20 @@ const PaneWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  padding: ${props => props.theme.units.unit}px;
 `
 
 const PaneHalf = styled.div`
   width: 50%;
   ${media.tablet`width:100%;`};
+  padding: ${props => props.theme.units.unit}px;
 `
 
 const PaneHalfFeatured = styled.div`
   width: 100%;
   ${media.tablet`width:100%;`};
+  padding: ${props => props.theme.units.unit}px;
 `
-//  @at-root & .content-section-pane-wrapper {
-//    padding: $unit;
 
 const PaneFlex = styled.div`flex: 1;`
 
@@ -146,34 +150,44 @@ const PaneGraphic = styled.div`
   ${media.tablet`width:35%;`} ${media.handheld`width:100%;`};
 `
 
-const PaneAlignLeft = styled.div`@at-root & .content-section-pane-wrapper {}`
+const AlignCenterContentSectionTitle = styled.div`max-width: 700px !important;`
 
-const PaneAlignCenter = styled.div`
-  @at-root & .wiggle-after {
-    &::after {
-      left: calc(50% - 30.5px);
-    }
-  }
-  @at-root & .content-section-pane-wrapper {
-    justify-content: center !important;
-    align-items: center !important;
-    text-align: center !important;
-  }
-  .content-section-title {
-    max-width: 700px !important;
-    @include handheld {
-      &-wrapper {
-        text-align: center !important;
-      }
-    }
-  }
-  p {
-    margin-left: auto !important;
-    margin-right: auto !important;
-    max-width: 600px !important;
-    @include handheld {
-      text-align: center;
-    }
+const AlignCenterContentSectionTitleWrapper = styled.div`
+  ${media.handheld` text-align: center !important`};
+`
+
+const AlignCenterContentP = styled.p`
+  margin-left: auto !important;
+  margin-right: auto !important;
+  max-width: 600px !important;
+  ${media.handheld`text-align:center`};
+`
+
+const StyleGraphite = styled.div`
+  background: ${props => props.theme.colors.graphitebase};
+`
+
+const CardSimple = styled.div`
+  position: relative;
+  margin-top: ${props => prop.theme.units.unit * 2}px;
+  min-width: 480px;
+`
+
+const CardSimpleWrapper = styled.div`
+  padding: ${props => props.theme.units.unit * 2}px;
+`
+
+const CardSimpleBody = styled.div`
+  text-align: center;
+  &::before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+    left: 0;
+    height: 100%;
+    border-radius: 40px;
+    box-shadow: 0 8px 15px rgba(80, 103, 132, .15);
   }
 `
 
