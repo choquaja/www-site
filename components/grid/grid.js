1ec1b8fb3882d2f8c721878b0d1f6ad71d8f6adc
choquaja <choquaja@whitman.edu>
2017-08-08 19:40:03 -0400

    started on grid

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -1,5 +1,83 @@
 import React from 'react'
 import styles from './grid.styles.scss'
+import styled from 'styled-components'
+import { centerContent, media } from '../../styles/helpers'
+
+const GridMain = styled.div`width: 100%;`
+
+const GridWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  overflow: hidden;
+  ${props => props.center && 'justify-content:center align-items:center'};
+`
+
+const Item = styled.div`
+  margin-bottom: ${props => props.theme.units.unit * 2}px;
+  ${media.tablet`width: 50% !important max-width:50% !important`};
+`
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const ItemGraphic = styled.div`width: 90px;`
+
+const ItemLabel = styled.div`
+  padding-top: ${props => props.theme.units.unit}px;
+  font-size: 1rem;
+  text-align: center;
+`
+
+const GridItemAvatar = styled.div`
+  min-width: 120px;
+  max-width: 140px;
+`
+
+const People = styled.div`
+  margin-top: ${props => props.theme.units.gutter * 4}px;
+`
+
+const PersonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: ${props => props.theme.units.gutter * 2};
+`
+
+const PersonAvatar = styled.div`
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  background: white;
+  margin-bottom: ${props => props.theme.units.gutter};
+  box-shadow: ${props => props.theme.animations.dropShadowShallow};
+  overflow: hidden;
+`
+
+const PersonDetailsName = styled.div`
+  font-size: 2rem;
+  font-family: ${props => props.theme.fonts.headings};
+  color: ${props => props.theme.colors.graphitebase};
+  font-weight: 100;
+`
+
+const PersonDetailsTitle = styled.div`
+  padding-top: ${props => props.theme.units.unit}px;
+  padding-bottom: ${props => props.theme.units.unit}px;
+  color: rgba(${props => props.theme.colors.graphitebase}, 0.75);
+`
+
+const PersonDetailsBio = styled.div`
+  max-width: 80%;
+  font-weight: 300;
+  opacity: 0.75;
+  ${centerContent};
+`
 
 export class GridItem extends React.Component {
   constructor(props) {
@@ -33,9 +111,9 @@ export class GridItem extends React.Component {
     }
     return (
       <div className={classes} style={gridItemStyles}>
-        <div className="grid-item-wrapper hidden">
+        <ItemWrapper>
           {this.props.children}
-        </div>
+        </ItemWrapper>
       </div>
     )
   }
@@ -70,14 +148,14 @@ export class Grid extends React.Component {
     return (
       <div className={classes}>
         <style dangerouslySetInnerHTML={{ __html: styles }} />
-        <div className="grid-wrapper">
+        <GridWrapper>
           {React.Children.map(this.props.children, child =>
             React.cloneElement(child, {
               columns: this.props.columns,
               style: this.props.itemStyle,
             })
           )}
-        </div>
+        </GridWrapper>
       </div>
     )
   }
